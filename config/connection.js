93aa88a4791d423cc27f061aa86429ac165eb8d3
George Sun <Gsunems@gmail.com>
2020-03-21 14:39:25 -0500

    changing bug

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "august16",
-        database: "burgers_db"
+        host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "b7d3if8lvdr272fx",
+        password: "rea4k5v54koe4z9h",
+        database: "	x8m5r80pdkjp57fn"
     });
 }
 connection.connect();
