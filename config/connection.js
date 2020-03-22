e09f4fc2fee4c153d57c4ea55db5bdb80385e119
George Sun <Gsunems@gmail.com>
2020-03-21 22:43:57 -0500

    ficing my connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,16 @@ const mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+    // connection = mysql.createConnection({
+    //     host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    //     user: "b7d3if8lvdr272fx",
+    //     password: "rea4k5v54koe4z9h",
+    //     database: "	x8m5r80pdkjp57fn"
     connection = mysql.createConnection({
-        host: "jw0ch9vofhcajqg7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-        user: "b7d3if8lvdr272fx",
-        password: "rea4k5v54koe4z9h",
-        database: "	x8m5r80pdkjp57fn"
+        host: "localhost:3306",
+        user: "root",
+        password: "august16",
+        database: "burgers_db"
     });
 }
 connection.connect();
